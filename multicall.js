97408ce06f824ab1e046967535cdc5f1e375bdd0
dragonfigherss <dragonfighter0111@outlook.com>
2023-07-09 10:15:08 -0400

    final code

--- multicall.js
+++ multicall.js
@@ -3,7 +3,7 @@ const { abi } = require("./AggregatorV3Interface.json");
 const priceFeedAddress = "0xDB8E00Ef74951874cEB59584483C9490fAE3747b";
 const ethers = require("ethers");
 require("dotenv").config();
-
+// final code
 async function main() {
   let provider = new ethers.providers.JsonRpcProvider(
     process.env.ALCHEMY_MAINNET_RPC_URL
